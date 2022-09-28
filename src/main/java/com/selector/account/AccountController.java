839654663898bc32f6d783bc2aa2024c4fa6c741
devilll <tkddud_2@naver.com>
2022-09-28 23:46:20 +0900

    AccountController Refactoring & TestCode Refactoring

--- src/main/java/com/selector/account/AccountController.java
+++ src/main/java/com/selector/account/AccountController.java
@@ -41,7 +41,8 @@ public class AccountController {
         if (errors.hasErrors()) {
             return "account/sign-up";
         }
-        accountService.processNewAccount(signUpForm);
+        Account account = accountService.processNewAccount(signUpForm);
+        accountService.login(account);
         return "redirect:/";
     }
 
@@ -54,13 +55,13 @@ public class AccountController {
             return view;
         }
 
-        if(!account.getEmailCheckToken().equals(token)){
+        if(!account.isValidToken(token)){
             model.addAttribute("error", "wrong token");
             return view;
         }
 
-        account.setEmailVerified(true);
-        account.setJoinedAt(LocalDateTime.now());
+        account.completeSignUp();
+        accountService.login(account);
         model.addAttribute("numberOfUser", accountRepository.count());
         model.addAttribute("nickName", account.getNickName());
         return view;
