b36bd72a862907c41fec22941d8c35295e3b5dc6
devilll <tkddud_2@naver.com>
2022-10-05 01:57:25 +0900

    SignUpFormValidator update

--- src/main/java/com/selector/account/SignUpFormValidator.java
+++ src/main/java/com/selector/account/SignUpFormValidator.java
@@ -20,7 +20,7 @@ public class SignUpFormValidator implements Validator {
     public void validate(Object object, Errors errors){
         SignUpForm signUpForm = (SignUpForm) object;
         if(accountRepository.existsByEmail(signUpForm.getEmail())){
-            errors.rejectValue("eamil", "invalid.email", new Object[]{signUpForm.getEmail()}, "이미 사용중인 이메일입니다.");
+            errors.rejectValue("email", "invalid.email", new Object[]{signUpForm.getEmail()}, "이미 사용중인 이메일입니다.");
         }
 
         if(accountRepository.existsByNickName(signUpForm.getNickName())){
