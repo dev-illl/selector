e481e91d4e619fb16a8007eb11ee945cbfa25b0d
dev-devill <82079978+dev-devill@users.noreply.github.com>
2022-10-18 16:57:38 +0900

    profile-update form Second Update

--- src/main/java/com/selector/settings/Profile.java
+++ src/main/java/com/selector/settings/Profile.java
@@ -2,17 +2,19 @@ package com.selector.settings;
 
 import com.selector.domain.Account;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class Profile {
 
     private String introduce;
 
+    private String instagramUrl;
+
     private String occupation;
 
     private String interest;
-
-    private String instagramUrl;
 
     public Profile(Account account) {
         this.introduce = account.getIntroduce();
